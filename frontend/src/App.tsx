ebd2de11fff618c11294cc143693fe8c1a87fa13
rednes <528249+rednes@users.noreply.github.com>
2024-05-14 15:02:00 +0900

    Refactor CSS styles and update scripts in package.json
    This commit removes individual CSS files and combines them into a single style.css file to improve code organization. The scripts in package.json are also updated to adopt npm-run-all for parallel execution. Additionally, prettier is added to backend package.json for linting, "backend/.gitignore" is added to exclude node_modules, and an unused SVG file is deleted. A proper table display for posts is implemented in App.tsx. Lastly, minor adjustments are made to the mock database 'db.json.'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import './App.css';
+import './style.css';
 
-import { useEffect, useState } from 'react';
+import { type JSX, useEffect, useState } from 'react';
 
 const API_URL = import.meta.env.VITE_API_URL || '';
 
-function App() {
+function App(): JSX.Element {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchApi = async () => {
-      const response = await fetch(`${API_URL}posts`);
+      const response = await fetch(`${API_URL}/posts`);
       const result = await response.json();
       setPosts(result);
     };
@@ -18,7 +18,29 @@ function App() {
 
   return (
     <>
-      <div>{JSON.stringify(posts)}</div>
+      <h1>Sample Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post: any, i: number) => (
+            <tr key={i}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>{post.author}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <h2>Original JSON</h2>
+        <pre>{JSON.stringify(posts, null, 4)}</pre>
+      </div>
     </>
   );
 }

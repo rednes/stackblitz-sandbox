08dad14ce52891ef0a755e1d2b402702b1c01cfa
rednes <528249+rednes@users.noreply.github.com>
2024-05-29 12:05:03 +0900

    npm i -D @biomejs/biome

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
 import fastify from "fastify";
 
-const server = fastify();
+const main = async () => {
+  const server = fastify();
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
-});
+  server.get("/ping", async (request, reply) => {
+    return "pong\n";
+  });
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+  server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+};
+
+main();

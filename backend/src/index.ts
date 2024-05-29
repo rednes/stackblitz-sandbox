f8a1bbde663338867d93ece0183453379fd03bbb
rednes <528249+rednes@users.noreply.github.com>
2024-05-29 12:55:08 +0900

    Add Swagger UI support for API documentation
    This commit introduces the Swagger UI into the backend API server. Changes include implementing the fastify-swagger and fastify-swagger-ui packages that provide interactive API documentation. The '/ping' endpoint has been updated to return a structured response instead of a plain string, now aligns with the defined Swagger documentation for the endpoint. The changes also include updates to the package-lock.json file to reflect the added dependencies.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,26 @@
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import fastify from "fastify";
 
-const main = async () => {
+const main = async (): Promise<void> => {
   const server = fastify();
 
-  server.get("/ping", async (request, reply) => {
-    return "pong\n";
-  });
+  await server.register(fastifySwagger);
+  await server.register(fastifySwaggerUi);
+
+  server.get(
+    "/ping",
+    {
+      schema: {
+        response: {
+          200: { properties: { message: { type: "string" } } },
+        },
+      },
+    },
+    async (request, reply) => {
+      return { message: "pong" };
+    },
+  );
 
   server.listen({ port: 8080 }, (err, address) => {
     if (err) {
